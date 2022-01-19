f98386393a46fefc4d3de6228a0150c062a0d1e6
Adrian Furmanek <adrian.furm@gmail.com>
2022-01-19 14:23:18 +0100

    Register, Login, JwtToken,Startup class extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -21,13 +20,15 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();
             return users;
         }
 
-         [HttpGet("{userId}")]
+        [Authorize]
+        [HttpGet("{userId}")]
         public async Task<ActionResult<AppUser>> GetUserById(int userId)
         {
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
